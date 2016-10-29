393466fa6848a43d199007aa9941c488aea9044b
DCamaron1 <league@Mac-mini.local>
2016-10-29 11:32:34 -0700

    10-29

---
+++ Level0/src/Poteto.java
@@ -0,0 +1,23 @@
+import javax.swing.JOptionPane;
+
+public class Poteto {
+	public static void main(String[] args) {
+		int num = 1;
+		String answer = JOptionPane.showInputDialog(null, "Do you want to read a poem?");
+		if (answer.equals("yes")) {
+			String answer2 = JOptionPane.showInputDialog(null,"What do you want to count with?");
+			while (num < 8) {
+				if (num == 4) {
+					System.out.print(num+", ");
+					num = num + 1;
+				} else {
+					System.out.print(num  + " "+answer2+", ");
+					num = num + 1;
+				}
+			}
+			System.out.print("more");
+		} else {
+			JOptionPane.showMessageDialog(null, "Ok :(");
+		}
+	}
+}
