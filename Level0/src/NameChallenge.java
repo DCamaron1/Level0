4ae08b06a609d388d36b9ab6a806b8810165f594
DCamaron1 <league@Mac-mini-3.local>
2016-11-05 11:37:15 -0700

    lols

---
+++ Level0/src/NameChallenge.java
@@ -0,0 +1,12 @@
+
+public class NameChallenge {
+public static void main(String[] args) {
+//	for (int num =2; num <= 10; num=num+2) {
+	//	System.out.println(num);
+		
+//	}
+	for(int hi=99; hi>=94; hi=hi-1){
+		System.out.println(hi);
+	}
+}
+}
---
+++ Level0/src/YearsAlive.java
@@ -0,0 +1,14 @@
+
+public class YearsAlive {
+	public static void main(String[] args) {
+		int answer = 0;
+		while (answer <= 14) {
+			System.out.println(answer);
+			answer = answer + 1;
+		}
+		System.out.println("such fun   very wow   amaze   (^_^)");
+		for (int answer2 = 0; answer2 <=14; answer2++) {
+			System.out.println(answer2);
+		}
+	}
+}
