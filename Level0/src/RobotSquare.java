504c8c4358a1bef164cf384c711ffd7593cb5a2c
NY-Giants <league@Admins-Mac-mini.local>
2017-03-18 11:33:20 -0700

    3-18-17

--- Level0/src/RobotSquare.java
+++ Level0/src/RobotSquare.java
@@ -7,13 +7,32 @@ public class RobotSquare {
 		rob.penDown();
 		rob.setPenWidth(5);
 		for (int x = 1; x <= 6; x = x + 1) {
-			rob.setRandomPenColor();
+			rob.setPenColor(100,50,150);
 			rob.move(100);
 			rob.turn(60);
 		}
 		rob.turn(60);
-		rob.move(200);
+		rob.penUp();	
+		rob.move(100);
+		rob.penDown();
+		rob.setPenColor(100,0,0);
+		rob.move(100);
 		rob.turn(-120);
+		rob.move(100);
+		rob.turn(-60);
+		rob.move(100);
+		//rob.setRandomPenColor();
+		rob.turn(-120);
+		rob.move(100);
+		rob.setPenColor(100,50,150);
+		rob.turn(60);
+		rob.move(100);
+		rob.turn(-120);
+		rob.move(100);
+		rob.turn(-120);
+		rob.move(100);
+		rob.setPenColor(100,0,0);
+		rob.turn(60);
 		rob.move(100);
 	}
 }
